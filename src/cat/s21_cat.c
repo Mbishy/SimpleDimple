646e978871f85b77eb253f7c691b9dd2ba97db2a
Mbishy work <mbishy@gp4surgut.ru>
2025-12-08 16:58:08 +0500

    add getline

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -7,43 +7,48 @@ typedef struct arguments {
 
 arguments *argument_parser(int argc, char *argv[]) {
   arguments arg = {0};
+  struct option long_options[] = {
+    {"number-nonblank", no_argument, NULL, 'b'},
+    {"number", no_argument, NULL, 'n'},
+    {"squeeze-blank", no_argument, NULL, 's'},
+    {0,0,0,0}
+  };
   int opt;
-  opt = getopt_long(argc, argv, "bnsEeTtv", NULL, 0);
+  opt = getopt_long(argc, argv, "bnsEeTt", long_options, 0);
   switch (opt) {
     case 'b':
       arg.b = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 'n':
+      arg.n = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 's':
+      arg.s = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 'e':
+      arg.E = 1;
+      arg.v = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 'E':
+      arg.E = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 't':
+      arg.T = 1;
+      arg.v = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
+    case 'T':
+      arg.T = 1;
       break;
 
-    case 'b':
-      arg.b = 1;
-      break;
-
-    case 'b':
-      arg.b = 1;
+    case '?':
+      perror("ERROR");
+      exit(1);
       break;
 
     default:
@@ -51,4 +56,23 @@ arguments *argument_parser(int argc, char *argv[]) {
   }
 }
 
-int main(int argc, char *argv[]) { return 0; }
+void outline(char* line, int n) {
+  for (int i = 0; i < n; i++)
+  {
+    putchar(line[i]);
+  }
+}
+
+void readfile() {
+
+}
+
+int main(int argc, char *argv[]) { 
+
+char* line = NULL;
+size_t memline = 0;
+int read = 0;
+getline();
+
+return 0; 
+}
